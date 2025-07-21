908416042dea0db702125d9b7675f59f02366cd7
Erik-solutions <info@erik19-analytics.com>
2025-07-21 11:07:16 +0000

    cors

--- server/backend/src/index.ts
+++ server/backend/src/index.ts
@@ -4,7 +4,11 @@ import formRoutes from "./routes/formRoutes";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use("/api/forms", formRoutes);
 
 
